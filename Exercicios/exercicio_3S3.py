3239f476aefb8405607b19f536d77c606b5086b3
Gustavo Mendes Pereira <118492103+GustavoMendesPereira@users.noreply.github.com>
2024-10-12 20:33:12 -0300

    Exercicio.
    Atividades

---
+++ Exercicios/exercicio_3S3.py
@@ -0,0 +1,7 @@
+
+valor1: int = int(input("Digite o primeiro valor:"))
+valor2: int = int(input("Digite o segundo valor:"))
+valor3: int = int(input("Agora digite o terceiro valor:"))
+
+resultado: int = (valor1 * valor1) + (valor2 * valor2) + (valor3 * valor3)
+print(f"A soma dos quadrados de {valor1} com {valor2} e {valor3} e igual a : {resultado}")
---
+++ Exercicios/exercico_2_S3.py
@@ -0,0 +1,10 @@
+"""
+2. Faça um programa que peça para o usuário digitar três valores inteiro e imprima a soma deles.
+"""
+
+numero1: int = int(input("Digite seu primeiro numero:"))
+numero2: int = int(input("Digite seu segundo numero:"))
+numero3: int = int(input("Agora digite o terceiro numero:"))
+
+resultado: int = numero1 + numero2 + numero3
+print(f"A soma dos numero {numero1} com {numero2} e {numero3} e igual a {resultado}")
